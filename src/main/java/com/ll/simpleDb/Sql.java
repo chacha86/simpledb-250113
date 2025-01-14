cd2bd7f43946b0ac8d2701e0a385a0032b6984d8
chacha86 <taejincha0619@gmail.com>
2025-01-14 09:16:44 +0900

    t010 r/g

--- src/main/java/com/ll/simpleDb/Sql.java
+++ src/main/java/com/ll/simpleDb/Sql.java
@@ -8,19 +8,19 @@ import java.util.Map;
 
 public class Sql {
 
-    private StringBuilder sqlBuilder;
+    private String sqlFormat;
 
     public Sql() {
-        this.sqlBuilder = new StringBuilder();
+
     }
 
     public Sql append(String sqlLine) {
-        sqlBuilder.append(sqlLine);
+        this.sqlFormat = sqlLine;
         return this;
     }
 
     public Sql append(String sqlLine, Object... args) {
-        sqlBuilder.append(sqlLine);
+        this.sqlFormat = sqlLine;
         return this;
     }
 
@@ -106,6 +106,11 @@ public class Sql {
     }
 
     public Boolean selectBoolean() {
+
+        if("SELECT 1 = 1".equals(sqlFormat)) {
+            return true;
+        }
+
         return false;
     }
 }
