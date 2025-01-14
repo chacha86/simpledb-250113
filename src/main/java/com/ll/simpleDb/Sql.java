aff35c97beba351cea34e68a1c9a6a8b3d31c4b7
chacha86 <taejincha0619@gmail.com>
2025-01-14 15:36:47 +0900

    t015 b

--- src/main/java/com/ll/simpleDb/Sql.java
+++ src/main/java/com/ll/simpleDb/Sql.java
@@ -42,8 +42,7 @@ public class Sql {
         return simpleDb.delete(sqlBuilder.toString(), params);
     }
 
-    public List<Article> selectRows(Class<?> cls) {
-
+    public <T> List<T> selectRows(Class<T> cls) {
         return simpleDb.selectRows(sqlBuilder.toString(), params, cls);
     }
 
