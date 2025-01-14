e185f8ce3cc1dcfd2d7765f95bd3836dcd0f8d95
chacha86 <taejincha0619@gmail.com>
2025-01-14 15:49:09 +0900

    t015 b2

--- src/test/java/com/ll/simpleDb/Article.java
+++ src/test/java/com/ll/simpleDb/Article.java
@@ -1,11 +1,11 @@
 package com.ll.simpleDb;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
-import java.util.Map;
 
 @AllArgsConstructor
 @Getter
@@ -17,18 +17,7 @@ public class Article {
     private String body;
     private LocalDateTime createdDate;
     private LocalDateTime modifiedDate;
+    @JsonProperty("isBlind")
     private boolean isBlind;
-
-    public static Article fromMap(Map<String, Object> map) {
-        return new Article(
-            (long) map.get("id"),
-            (String) map.get("title"),
-            (String) map.get("body"),
-            (LocalDateTime) map.get("createdDate"),
-            (LocalDateTime) map.get("modifiedDate"),
-            (boolean) map.get("isBlind")
-        );
-
-    }
 
 }
