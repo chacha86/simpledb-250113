d71b6ba21ba085ad037c2af1650962d59a1a3edb
chacha86 <taejincha0619@gmail.com>
2025-01-15 09:23:45 +0900

    t017 g

--- src/main/java/com/ll/simpleDb/SimpleDb.java
+++ src/main/java/com/ll/simpleDb/SimpleDb.java
@@ -174,5 +174,11 @@ public class SimpleDb {
     }
 
     public void close() {
+
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
