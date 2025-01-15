24213fe9f20d86b2385248df6c76d148fd2cd019
chacha86 <taejincha0619@gmail.com>
2025-01-15 10:22:03 +0900

    t017, t018 b

--- src/main/java/com/ll/simpleDb/SimpleDb.java
+++ src/main/java/com/ll/simpleDb/SimpleDb.java
@@ -10,7 +10,8 @@ public class SimpleDb {
     private String dbUrl;
     private String dbUser;
     private String dbPassword;
-    private Connection connection;
+    private Map<String, Connection> connections;
+
     @Setter
     private boolean devMode = false;
 
@@ -20,16 +21,28 @@ public class SimpleDb {
         this.dbUser = user;                                    // 사용자 이름
         this.dbPassword = password;                            // 비밀번호
 
-        // 연결 초기화
+        connections = new HashMap<>();
+
+    }
+
+    private Connection getCurrentThreadConnection() {
+
         try {
-            connection = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
-            if (devMode) {
-                System.out.println("데이터베이스에 성공적으로 연결되었습니다.");
+            Connection conn = connections.get(Thread.currentThread().getName());
+
+            if (conn == null) {
+                Connection currentThreadConn = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+                connections.put(Thread.currentThread().getName(), currentThreadConn);
+
+                return currentThreadConn;
             }
+
+            return conn;
         } catch (SQLException e) {
-            throw new RuntimeException("데이터베이스 연결 실패: " + e.getMessage());
+            throw new RuntimeException(e);
         }
     }
+
 
     public long insert(String sql, List<Object> params) {
         return _run(sql, Long.class, params);
@@ -88,6 +101,8 @@ public class SimpleDb {
 
     private <T> T _run(String sql, Class<T> cls, List<Object> params) {
         System.out.println("sql : " + sql);
+
+        Connection connection = getCurrentThreadConnection();
         try (PreparedStatement stmt = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
             setParams(stmt, params);
 
@@ -176,7 +191,8 @@ public class SimpleDb {
     public void close() {
 
         try {
-            connection.close();
+            Connection conn = getCurrentThreadConnection();
+            conn.close();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -184,7 +200,7 @@ public class SimpleDb {
 
     public void startTransaction() {
         try {
-            connection.setAutoCommit(false); // auto commit 끄기
+            getCurrentThreadConnection().setAutoCommit(false); // auto commit 끄기
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -192,7 +208,7 @@ public class SimpleDb {
 
     public void rollback() {
         try {
-            connection.rollback();
+            getCurrentThreadConnection().rollback();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
